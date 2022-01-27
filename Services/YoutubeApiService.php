ac57c195d9bd01b55bfd5a02c8d845d2a05dc0a2
Jakob Bennemann <jakob@raidboxes.de>
2022-01-27 18:32:52 +0100

    feat: add method to get playlist information

--- Services/YoutubeApiService.php
+++ Services/YoutubeApiService.php
@@ -45,4 +45,49 @@ class YoutubeApiService
             'subscribers_count' => $response->json('items.0.statistics.subscriberCount'),
         ];
     }
+
+    public function getVideoInformationForPlaylist(
+        string $playlistId,
+        int $limit = 10
+    ): Collection
+    {
+        //compose endpoint url for API
+        $apiUrl = sprintf(
+            '%s%s?part=%s&playlistId=%s&maxResults=%s&key=%s',
+            self::API_BASE_URL,
+            'playlistItems',
+            'snippet',
+            $playlistId,
+            $limit,
+            $this->apiKey
+        );
+
+        //perform GET request to API
+        $response = Http::withHeaders(['Accept' => 'application/json'])
+            ->get($apiUrl);
+
+        //initialize collection to store results in
+        $videos = collect();
+
+        //compose all information from api
+        foreach($response->json('items') as $item) {
+            $videoTitle = $item['snippet']['title'];
+            $videoDescription = $item['snippet']['description'];
+            $videoId = $item['snippet']['resourceId']['videoId'];
+            $videoThumbnail = $item['snippet']['thumbnails']['high']['url'];
+            $datePublished = $item['snippet']['publishedAt'];
+            $iframeMarkup = '<iframe width="560" height="315" src="https://www.youtube.com/embed/'.$videoId.'" title="'.$videoTitle.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+            $videos->add([
+                'id' => $videoId,
+                'title' => $videoTitle,
+                'description' => $videoDescription,
+                'thumbnail_url' => $videoThumbnail,
+                'published_at' => $datePublished,
+                'iframe' => $iframeMarkup,
+            ]);
+        }
+
+        //return all videos
+        return $videos;
+    }
 }
